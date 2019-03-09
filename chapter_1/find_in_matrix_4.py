e35206d9f65e4ab10b7dcb36e0b2321543decf8f
xiaoliji <yutian9527@gmail.com>
2019-03-09 14:22:35 +0800

    feat: add question 4

---
+++ chapter_1/find_in_matrix_4.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time     : 2019/3/9 1:30 PM
+# @Author   : xiaoliji
+# @Email    : yutian9527@gmail.com
+
+"""
+    在从左到右从上到下递增的矩阵中，判断目标数是否存在
+
+    >>> m = [[1, 2, 8, 9], [2, 4, 9, 12], [4, 7, 10, 13], [6, 8, 11, 15]]
+    >>> search_in_matrix(7)
+    >>> True
+    >>> search_in_matrix(5)
+    >>> False
+"""
+
+
+def search_in_matrix(target: int, matrix: 'List[List[int]]') -> 'bool':
+
+    row = 0
+    col = len(matrix[0]) - 1
+    while col >= 0 and row <= len(matrix) - 1:
+        if matrix[row][col] > target:
+            col -= 1
+        elif matrix[row][col] < target:
+            row += 1
+        else:
+            return True
+    return False
+
+
